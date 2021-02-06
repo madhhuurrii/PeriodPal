5e35253ef0e33bb79219dc77a7138150d7f08cf5
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2021-02-06 22:06:21 +0530

    PreFinal Changes

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('event-type', function (event) {
+  // Handle event
+  element: {
+    title: string,
+    subtitle: string,
+    image: {
+      src: {rawUrl: string}
+    },
+    text: string
+  }
+});
